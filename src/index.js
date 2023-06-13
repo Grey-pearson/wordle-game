0b6f0d72e5ff95c9b3e676267168e99deacef8a7
Grey P <greytpearson@gmail.com>
2023-06-12 22:44:13 -0600

    started base for wordle game

--- src/index.js
+++ src/index.js
@@ -9,3 +9,9 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// WORDLE
+// grid of square cells 5W*6H
+// guesses must be a word in the word list and 5 letters long
+// once full word is entered and enter button is hit render letters in the work and in the right place green and letters that are right but in the wrong place as yellow
+// statistics are shown in the pop up you get when finishing the wordle
